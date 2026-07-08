2d2346dad36d80cf58f19f64e5c58f376f10fff3
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 12:20:38 -0300

    add

--- ex_09.php
+++ ex_09.php
@@ -6,5 +6,19 @@ function analisarNumero($numero) {
     } else {
         return "O número é ímpar.";
     }
+
+    if ($numero > 0) {
+        return "O número é primo.";
+    } else {
+        return "O número não é primo.";
+    }
+
+    if ($numero > 0 && $numero % 2 === 0) {
+        return "O número é perfeito.";
+    } elseif ($numero < 0) {
+        return "O número não é perfeito.";
+    } else {
+        return "O número é zero.";
+    }
 }
 
