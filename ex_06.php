15e99b4b24c2ed1efec28c60d770d5400f225326
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 12:11:30 -0300

    add

--- ex_06.php
+++ ex_06.php
@@ -5,7 +5,11 @@ function converterTemperatura($temperatura, $unidadeOrigem, $unidadeDestino) {
         return ($temperatura * 9/5) + 32;
     } elseif ($unidadeOrigem === 'F' && $unidadeDestino === 'C') {
         return ($temperatura - 32) * 5/9;
+    } elseif ($unidadeOrigem === 'C' && $unidadeDestino === 'K') {
+        return $temperatura + 273.15;
+    } elseif ($unidadeOrigem === 'K' && $unidadeDestino === 'C') {
+        return $temperatura - 273.15;
     } else {
-        return "Unidades inválidas. Use 'C' para Celsius e 'F' para Fahrenheit.";
+        return "Unidades inválidas. Use 'C' para Celsius,'F' para Fahrenheit e 'K' para Kelvin.";
     }
 }
