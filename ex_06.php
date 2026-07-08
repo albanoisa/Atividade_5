0f36fc086b3e517b48d863b1af46db1151c8b854
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 10:00:26 -0300

    add

--- ex_06.php
+++ ex_06.php
@@ -1,3 +1,11 @@
 <?php
 
-function
+function converterTemperatura($temperatura, $unidadeOrigem, $unidadeDestino) {
+    if ($unidadeOrigem === 'C' && $unidadeDestino === 'F') {
+        return ($temperatura * 9/5) + 32;
+    } elseif ($unidadeOrigem === 'F' && $unidadeDestino === 'C') {
+        return ($temperatura - 32) * 5/9;
+    } else {
+        return "Unidades inválidas. Use 'C' para Celsius e 'F' para Fahrenheit.";
+    }
+}
