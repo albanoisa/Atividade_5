a0ef907671e74dcdc2cfbea545968e02806838e9
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 12:36:37 -0300

    add

--- ex_14.php
+++ ex_14.php
@@ -1,3 +1,18 @@
 <?php
 
-function
+function estatisticasNumericas($numeros) {
+    $soma = array_sum($numeros);
+    $media = $soma / count($numeros);
+    $maior = max($numeros);
+    $menor = min($numeros);
+
+    return [
+        'soma' => $soma,
+        'media' => $media,
+        'maior' => $maior,
+        'menor' => $menor,
+        'mediana' => calcularMediana($numeros),
+        'quantidadepares' => contarPares($numeros),
+        'quantidadedeimpares' => contarImpares($numeros)
+    ];
+}
