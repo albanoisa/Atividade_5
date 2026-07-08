7c3b207424093803fba391330dfaadcb16a19126
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 12:23:36 -0300

    add

--- ex_10.php
+++ ex_10.php
@@ -6,4 +6,13 @@ function calcularMedia($numeros) {
     }
     $soma = array_sum($numeros);
     return $soma / count($numeros);
+
+    if ($media >= 7) {
+        return "Aprovado";
+    } elseif ($media >= 5) {
+        return "Recuperação";
+    } else {
+        elseif ($media < 5) {
+        return "Reprovado";
+    }
 }
