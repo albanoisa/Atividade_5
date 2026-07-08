8994f94ed2c8bf7ff48900b9ed7a47f1bd1e43a1
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 10:03:22 -0300

    add

--- ex_10.php
+++ ex_10.php
@@ -1,3 +1,9 @@
 <?php
 
-function
+function calcularMedia($numeros) {
+    if (count($numeros) === 0) {
+        return 0;
+    }
+    $soma = array_sum($numeros);
+    return $soma / count($numeros);
+}
