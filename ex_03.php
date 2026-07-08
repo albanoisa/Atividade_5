9bc4dc3b08d908d55bfbdd69a891b7549c4e0fba
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 09:53:08 -0300

    add

---
+++ ex_03.php
@@ -0,0 +1,8 @@
+function mascaraCPF($cpf) {
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+    return substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 6, 3) . '-' . substr($cpf, 9, 2);
+}
+
