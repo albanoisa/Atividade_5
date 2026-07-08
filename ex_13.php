1c653276373d77171dcd08e5ff6a319b55c944e1
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 11:50:32 -0300

    add

--- ex_13.php
+++ ex_13.php
@@ -14,3 +14,6 @@ function criptografarMensagem($mensagem, $chave) {
 
     return base64_encode($mensagemCriptografada);
 }
+
+function descriptografarMensagem($mensagemCriptografada, $chave) {
+    $mensagemCriptografada = base64_decode
