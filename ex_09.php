eaf464b045318c0e230add05872d70ec31020f9d
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 12:18:17 -0300

    add

--- ex_09.php
+++ ex_09.php
@@ -1,11 +1,10 @@
 <?php
 
 function analisarNumero($numero) {
-    if ($numero > 0) {
-        return "O número é positivo.";
-    } elseif ($numero < 0) {
-        return "O número é negativo.";
+    if ($numero % 2 === 0) {
+        return "O número é par.";
     } else {
-        return "O número é zero.";
+        return "O número é ímpar.";
     }
 }
+
