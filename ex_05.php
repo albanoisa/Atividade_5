d73b4c30ef204803e260cc0fb2d28bafc04650c2
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 11:57:59 -0300

    add

--- ex_05.php
+++ ex_05.php
@@ -4,6 +4,7 @@ function analisarTexto($texto) {
     $palavras = str_word_count($texto, 1);
     $frequencia = array_count_values($palavras);
     $frequencia = array_count_values($caracteres);
+    $frequencia = array_count_values($vogais);
     arsort($frequencia);
     return $frequencia;
 }
