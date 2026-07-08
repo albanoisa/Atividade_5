b058cd7dcdb7e86b08dab6761a15c1efad7de652
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 11:51:26 -0300

    add

--- ex_13.php
+++ ex_13.php
@@ -18,3 +18,16 @@ function criptografarMensagem($mensagem, $chave) {
 function descriptografarMensagem($mensagemCriptografada, $chave) {
     $mensagemCriptografada = base64_decode($mensagemCriptografada);
     $mensagemDescriptografada = '';
+
+    $tamanhoMensagem = strlen($mensagemCriptografada);
+    $tamanhoChave = strlen($chave);
+
+    for ($i = 0; $i < $tamanhoMensagem; $i++) {
+        $caractereMensagem = ord($mensagemCriptografada[$i]);
+        $caractereChave = ord($chave[$i % $tamanhoChave]);
+        $caractereDescriptografado = ($caractereMensagem - $caractereChave + 256) % 256;
+        $mensagemDescriptografada .= chr($caractereDescriptografado);
+    }
+
+    return $mensagemDescriptografada;
+}
