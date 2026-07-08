b8024a1bd79f3112830ccc0478c29ae68915ef91
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 10:01:08 -0300

    add

--- ex_07.php
+++ ex_07.php
@@ -1,3 +1,7 @@
 <?php
 
-function
+function calcularDesconto($precoOriginal, $percentualDesconto) {
+    $desconto = $precoOriginal * ($percentualDesconto / 100);
+    $precoFinal = $precoOriginal - $desconto;
+    return $precoFinal;
+}
