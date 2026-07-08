c2634fbb8dc42ff5f1e49ab2ce726ca66104d8f3
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-07-08 10:02:49 -0300

    add

--- ex_09.php
+++ ex_09.php
@@ -1,3 +1,11 @@
 <?php
 
-function
+function analisarNumero($numero) {
+    if ($numero > 0) {
+        return "O número é positivo.";
+    } elseif ($numero < 0) {
+        return "O número é negativo.";
+    } else {
+        return "O número é zero.";
+    }
+}
