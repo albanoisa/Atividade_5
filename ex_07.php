6a0d4ff8c3f76dc9b750345387d32f601d9ef112
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-07 11:12:19 -0300

    add

--- ex_07.php
+++ ex_07.php
@@ -1,10 +1,33 @@
 <?php
 
-function calcularDesconto($precoOriginal, $percentualDesconto) {
-    $desconto = $precoOriginal * ($percentualDesconto / 100);
-    $precoFinal = $precoOriginal - $desconto;
-    return $precoFinal;
-     return "Acima de R$ 100,00: 10% de desconto";
-     return "Acima de R$ 500,00: 20% de desconto";
-     return "Acima de R$ 1000,00: 30% de desconto";
+function calcularDesconto($valorCompra){
+
+    if ($valorCompra > 1000){
+        $percentualDesconto = 0.30;
+    } elseif ($valorCompra > 500){
+        $percentualDesconto = 0.20;
+    } elseif ($valorCompra > 100){
+        $percentualDesconto = 0.10;
+    } else {
+        $percentualDesconto = 0;
+    }
+
+    $valorDesconto = $valorCompra * $percentualDesconto;
+    $valorFinal = $valorCompra - $valorDesconto;
+
+    return [
+        "valor_original" => $valorCompra,
+        "desconto" => $valorDesconto,
+        "valor_final" => $valorFinal
+    ];
+
 }
+
+    $valor_usuario = 750;
+    $resultado = calcularDesconto($valor_usuario);
+
+    echo "Valor original: R$ " . number_format($resultado["valor_original"], 2, ",", ".") . "<br>";
+    echo "Desconto aplicado: R$ " . number_format($resultado["desconto"], 2, ",", ".") . "<br>";
+    echo "Valor final: R$ " . number_format($resultado["valor_final"], 2, ",", ".") . "<br>";
+
+?>
