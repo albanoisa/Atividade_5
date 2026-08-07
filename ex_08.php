5db57ffd2922da390fca7d08c998906de691b4cf
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-07 11:13:28 -0300

    add

--- ex_08.php
+++ ex_08.php
@@ -1,6 +1,25 @@
 <?php
 
-function ordenarNomes($nomes) {
-    sort($nomes);
-    return $nomes;
+function ordenarNomes($nomesTexto){
+
+    $vetorNomes = explode(",", $nomesTexto);
+    $vetorNomes = array_map("trim", $vetorNomes);
+
+    sort($vetorNomes);
+
+    return $vetorNomes;
+
 }
+    $nomes_usuario = "Carlos,  ana ,  Bruno,fernanda ,Daniela";
+
+    echo "Lista original: $nomes_usuario <br>";
+
+    $listaOrganizada = ordenarNomes($nomes_usuario);
+
+    echo "Lista organizada: <br>";
+
+    foreach ($listaOrganizada as $nome){
+    echo "- $nome <br>";
+}
+
+?>
