ab3d21c6c2b53eb0a786e038cfd35d81d61f1652
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-07 11:03:55 -0300

    add

--- ex_01.php
+++ ex_01.php
@@ -1,16 +1,21 @@
 <?php
 
 //(x² + y²) / (x + y)
-function calcularFormula ($x, $y)
-{
-if (($x + $y) = 0) {
-return "Não é possível realizar a divisão por zero.";
+ function calcularFormula($x,$y){
+
+    if (($x + $y) == 0){
+        return "Não foi possível realizar a divisão por zero!";
+    }
+   
+    $resultado = ((pow($x,2) + pow($y,2)) / ($x + $y));
+    return $resultado;
+
 }
-$resultado (pow($x, 2) + pow($y, 2)) / ($x + $y);
-return $resultado;
-}
-$x = 10;
-$y = 5
-echo "Valor de X: $x <br>";
-echo "Valor de Y: $y <br><br>";
-echo "Resultado: ". calcularFormula ($x, $y);
+   $x_usuario = 30;
+   $y_usuario = 50;
+
+   echo "Valor de X é: $x_usuario <br>";
+   echo "Valor de Y é: $y_usuario <br>";
+   echo calcularFormula($x_usuario,$y_usuario);
+
+?>
