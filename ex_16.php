273a6fbf2356946edf23242a223a2984322b06a3
isabelaalbano <isabela_albano@estudante.sesisenai.org.br>
2026-09-21 14:32:30 -0300

    exercicio 16 sendo feito

--- ex_16.php
+++ ex_16.php
@@ -4,3 +4,18 @@ function contarMaiusculas($senha)
 {
     return preg_match_all('/[A-Z]/', $senha);
 }
+
+function contarMinusculas($senha)
+{
+    return preg_match_all('/[a-z]/', $senha);
+}
+
+function contarNumeros($senha)
+{
+    return preg_match_all('/[0-9]/', $senha);
+}
+
+function contarCaracteresEspeciais($senha)
+{
+    return preg_match_all('/[^a-zA-Z0-9]/', $senha);
+}
